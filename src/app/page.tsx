ad7120cc0acd2c6ddfe209deb62301eee725ce36
Mohammad Alsallal <malslal787@gmail.com>
2024-09-27 16:09:53 +0300

    home page edits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Navbar from "@/components/global/navbar";
 import CardsWrapper from "@/components/home/cards-container";
-import Hero from "@/components/home/hero";
 
 const HomePage = () => {
   return (
     <main>
       <Navbar />
-      <Hero />
       <CardsWrapper />
     </main>
   );
--- src/components/home/cards-container.tsx
+++ src/components/home/cards-container.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { fetchDashboardData } from "@/actions/dashboard/fetch-dashboard-data";
 import { fetchHospitals } from "@/actions/dashboard/fetch-hospitals";
 import { useEffect, useState } from "react";
 import {
@@ -19,97 +18,98 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { fetchMedicines } from "@/actions/dashboard/fetch-medicine";
 
-// Define the types for data
-type DashboardData = {
-  totalHospitals: number;
-  totalMedicines: number;
-  lowStockAlerts: number;
-  unapprovedHospitals: number;
-};
+export interface Medicine {
+  id: string;
+  name: string;
+  stock: number;
+  minStock: number | null; // Allow null for minStock
+  expirationDate: Date;
+  hospitalId: string; // Added to filter medicines by hospital
+}
 
 export interface Hospital {
   id: string;
   name: string;
   address: string;
   phone: string;
-  status: string;
-  medData: { name: string; quantity: number }[]; // Example for medData
+  status: string | null; // Allow null for status
 }
 
 const CardsWrapper = () => {
-  const [dashboardData, setDashboardData] = useState<DashboardData | null>(
-    null
-  );
   const [hospitals, setHospitals] = useState<Hospital[]>([]);
   const [selectedHospital, setSelectedHospital] = useState<Hospital | null>(
     null
-  ); // State for selected hospital (to show in modal)
-  const [isModalOpen, setIsModalOpen] = useState(false); // State to track if modal is open
+  );
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [medicines, setMedicines] = useState<Medicine[]>([]);
 
   useEffect(() => {
     const loadHospitals = async () => {
       const data = await fetchHospitals();
-      setHospitals(
-        data.map((hospital) => ({
-          ...hospital,
-          status: hospital.status || "",
-          medData: hospital.medData || [], // Assuming medical data comes with hospital
-        }))
-      );
+      setHospitals(data);
     };
     loadHospitals();
   }, []);
 
-  useEffect(() => {
-    const loadDashboardData = async () => {
-      const data = await fetchDashboardData();
-      setDashboardData(data);
-    };
+  const loadMedicines = async (hospitalId: string) => {
+    const allMedicines = await fetchMedicines();
+    const hospitalMedicines = allMedicines.filter(
+      (med) => med.hospitalId === hospitalId
+    );
+    const sortedMedicines = hospitalMedicines.sort((a, b) => a.stock - b.stock);
+    setMedicines(sortedMedicines);
+  };
 
-    loadDashboardData();
-  }, []);
+  const getStockFlagColor = (stock: number) => {
+    if (stock === 0) {
+      return { color: "bg-red-600", status: "Out of Stock" };
+    } else if (stock <= 50) {
+      return { color: "bg-yellow-500", status: "Low Stock" };
+    } else if (stock < 200) {
+      return { color: "bg-orange-500", status: "Medium Stock" };
+    }
+    return { color: "bg-green-500", status: "In Stock" };
+  };
 
-  // Function to open modal and set selected hospital
   const handleMoreInfoClick = (hospital: Hospital) => {
     setSelectedHospital(hospital);
+    loadMedicines(hospital.id);
     setIsModalOpen(true);
   };
 
-  // Function to close modal
   const closeModal = () => {
     setIsModalOpen(false);
     setSelectedHospital(null);
+    setMedicines([]);
   };
 
   return (
-    <section className="container w-full mx-auto">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mx-auto mb-20">
-        {hospitals &&
-          hospitals.map((hospital) => (
-            <Card key={hospital.id} className="w-full mx-auto">
-              <CardHeader>
-                <CardTitle>{hospital.name}</CardTitle>
-                <CardDescription>{hospital.address}</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <p>Phone: {hospital.phone}</p>
-                <p>Status: {hospital.status}</p>
-              </CardContent>
-              <CardFooter>
-                <Button onClick={() => handleMoreInfoClick(hospital)}>
-                  More Info
-                </Button>
-              </CardFooter>
-            </Card>
-          ))}
+    <section className="flex items-center justify-center w-full h-screen overflow-y-auto max-sm:mt-24 mb-16">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 max-h-screen p-4">
+        {hospitals.map((hospital) => (
+          <Card key={hospital.id} className="w-full mx-auto">
+            <CardHeader>
+              <CardTitle>{hospital.name}</CardTitle>
+              <CardDescription>{hospital.address}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Phone: {hospital.phone}</p>
+              <p>Status: {hospital.status || "N/A"}</p>
+            </CardContent>
+            <CardFooter>
+              <Button onClick={() => handleMoreInfoClick(hospital)}>
+                More Info
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
       </div>
 
-      {/* Dark Layer and Modal for showing more hospital details */}
       {isModalOpen && selectedHospital && (
         <div className="fixed inset-0 z-[99999] flex items-center justify-center bg-black bg-opacity-70">
           <div className="bg-neutral-900 w-4/5 h-4/5 p-8 rounded-lg shadow-lg overflow-y-auto relative">
-            {/* Close Button */}
             <button
               className="absolute top-4 right-4 text-lg font-bold text-white hover:text-red-400 transition-colors duration-200"
               onClick={closeModal}
@@ -117,7 +117,6 @@ const CardsWrapper = () => {
               X
             </button>
 
-            {/* Hospital Details */}
             <h2 className="text-2xl font-bold mb-4 text-white">
               {selectedHospital.name} Details
             </h2>
@@ -128,10 +127,9 @@ const CardsWrapper = () => {
               <strong>Phone:</strong> {selectedHospital.phone}
             </p>
             <p className="mb-4 text-white">
-              <strong>Status:</strong> {selectedHospital.status}
+              <strong>Status:</strong> {selectedHospital.status || "N/A"}
             </p>
 
-            {/* Medical Data Table */}
             <h3 className="text-xl font-semibold mb-4 text-white">
               Medicine Stock:
             </h3>
@@ -144,27 +142,47 @@ const CardsWrapper = () => {
                   <TableHead className="text-left border-b border-gray-700">
                     Quantity
                   </TableHead>
+                  <TableHead className="text-left border-b border-gray-700">
+                    Status
+                  </TableHead>
+                  <TableHead className="text-left border-b border-gray-700">
+                    Nearest Expiration Date
+                  </TableHead>
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {selectedHospital.medData.length > 0 ? (
-                  selectedHospital.medData.map((med, index) => (
-                    <TableRow key={index} className="hover:bg-neutral-800">
-                      <TableCell className="border-b border-gray-700">
-                        {med.name}
-                      </TableCell>
-                      <TableCell className="border-b border-gray-700">
-                        {med.quantity}
-                      </TableCell>
-                    </TableRow>
-                  ))
+                {medicines.length > 0 ? (
+                  medicines.map((med) => {
+                    const { color, status } = getStockFlagColor(med.stock);
+                    return (
+                      <TableRow key={med.id} className="hover:bg-neutral-800">
+                        <TableCell className="border-b border-gray-700">
+                          {med.name}
+                        </TableCell>
+                        <TableCell className="border-b border-gray-700">
+                          {med.stock}
+                        </TableCell>
+                        <TableCell className="border-b border-gray-700">
+                          <div className="flex items-center">
+                            <div
+                              className={`inline-block w-3 h-3 rounded-full ${color}`}
+                            ></div>
+                            <span className="ml-2">{status}</span>
+                          </div>
+                        </TableCell>
+                        <TableCell className="border-b border-gray-700">
+                          {new Date(med.expirationDate).toLocaleDateString()}
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })
                 ) : (
                   <TableRow>
                     <TableCell
-                      colSpan={2}
+                      colSpan={4}
                       className="border-b border-gray-700 text-center"
                     >
-                      No medical data available.
+                      No medicines available.
                     </TableCell>
                   </TableRow>
                 )}
